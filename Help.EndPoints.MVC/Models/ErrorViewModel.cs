ee329c3382b64ddfb7a6a54aff7bcb639a18938c
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-04-26 01:09:50 -0700

    Created the structure class libraries.

---
+++ Help.EndPoints.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Help.EndPoints.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
