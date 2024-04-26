5abbc845ab8b62e5eed2cdc47ed7863c2526f7d8
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-04-26 01:19:16 -0700

    Wrote the BaseEntity.

---
+++ Base_Framework/Domain/Core/Entities/BaseEntity.cs
@@ -0,0 +1,27 @@
+﻿
+
+namespace Base_Framework.Domain.Core.Entities
+{
+    public class BaseEntity
+    {
+        public BaseEntity()
+        {
+            IsRemoved = false;
+            CreationDate = DateTime.Now;
+        }
+
+        public long Id { get; set; }
+        public bool IsRemoved { get; set; }
+        public DateTime CreationDate { get; set; }
+
+        public void Remove()
+        {
+            IsRemoved = true;
+        }
+
+        public void ReStore()
+        {
+            IsRemoved = false;
+        }
+    }
+}
