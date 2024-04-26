de63245a5f70364a2656f122f746f278b7f4e83b
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-04-26 07:47:12 -0700

    Wrote the ServicePicture entity from Service Aggregate.

---
+++ Help.Domain.Core/ServiceAgg/Entities/ServicePicture.cs
@@ -0,0 +1,14 @@
+﻿
+
+using Base_Framework.Domain.Core.Entities;
+
+namespace Help.Domain.Core.ServiceAgg.Entities
+{
+    public class ServicePicture : BaseEntity
+    {
+        public string Name { get; private set; }
+        public string Title { get; private set; }
+        public string Alt { get; private set; }
+        public long ServiceId { get; private set; }
+    }
+}
