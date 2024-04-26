e718ca94f9c7a7b9de98dad615f3c2c4e674c269
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-04-26 05:30:51 -0700

    Wrote Skill entity from Service Aggragate.

---
+++ Help.Domain.Core/ServiceAgg/Entities/Skill.cs
@@ -0,0 +1,32 @@
+﻿using Base_Framework.Domain.Core.Entities;
+
+namespace Help.Domain.Core.ServiceAgg.Entities
+{
+    public class Skill : BaseEntity
+    {
+        public Skill(string title, string description, Int16 level, long serviceId, long categoryId)
+        {
+            Title = title; 
+            Description = description; 
+            Level = level; 
+            ServiceId = serviceId; 
+            CategoryId = categoryId;
+            
+        }
+
+        public string Title { get; private set; }
+        public string Description { get; private set; }
+        public Int16 Level { get; private set; }
+        public long ServiceId { get; private set; }
+        public long CategoryId { get; private set; }
+
+        public void Edit(string title, string description, Int16 level, long serviceId, long categoryId)
+        {
+            Title = title;
+            Description = description;
+            Level = level;
+            ServiceId = serviceId;
+            CategoryId = categoryId;
+        }
+    }
+}
