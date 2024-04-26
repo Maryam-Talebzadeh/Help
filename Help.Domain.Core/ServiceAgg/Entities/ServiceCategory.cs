547ffbd8460f17703d339ec66c6529990377f893
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-04-26 02:08:34 -0700

    Wrote ServiceCategory from the Service Aggregate.

---
+++ Help.Domain.Core/ServiceAgg/Entities/ServiceCategory.cs
@@ -0,0 +1,26 @@
+﻿using Base_Framework.Domain.Core.Entities;
+
+namespace Help.Domain.Core.ServiceAgg.Entities
+{
+    public class ServiceCategory : BaseEntity
+    {
+        public ServiceCategory(string title, string description, long parentId)
+        {
+            Title = title;
+            Description = description;
+            ParentId = parentId;
+        }
+
+        public string Title { get; private set; }
+        public string Description { get; private set; }
+        public long ParentId { get; private set; }
+        public ServiceCategory Parrent { get; private set; }
+
+        public void Edit(string title, string description, long parentId)
+        {
+            Title = title;
+            Description = description;
+            ParentId = parentId;
+        }
+    }
+}
