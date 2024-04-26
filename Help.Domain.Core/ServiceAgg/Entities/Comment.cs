6e1895fba1d8803731c55b13e93acecd6e99cc7a
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-04-26 08:28:29 -0700

    Wrote Comment entity from Service Aggregate.

---
+++ Help.Domain.Core/ServiceAgg/Entities/Comment.cs
@@ -0,0 +1,35 @@
+﻿using Base_Framework.Domain.Core.Entities;
+
+namespace Help.Domain.Core.ServiceAgg.Entities
+{
+    public class Comment : BaseEntity
+    {
+        public Comment(string message, Int16 score, long parentId, long helpRequestId, long applicantId, long expertId)
+        {
+            Message = message; 
+            Score = score; 
+            ParentId = parentId;
+            IsConfirmed = false;
+            HelpRequestId = helpRequestId;
+            ApplicantId = applicantId;
+            ExpertId = expertId;
+        }
+
+        public long HelpRequestId { get; private set; }
+        public long ApplicantId { get; private set; }
+        public long ExpertId { get; private set; }
+        public string Message { get; private set; }
+        public bool IsConfirmed { get; private set; }
+        public Int16 Score { get; private set; }
+        public long ParentId { get; private set; }
+        public Comment Parent { get; private set; }
+
+        public void Edit(string message, Int16 score, long parentId)
+        {
+            Message = message;
+            Score = score;
+            ParentId = parentId;
+        }
+
+    }
+}
