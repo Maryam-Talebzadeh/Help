29a739ac84eb0ee64b033df1b8b91a763d49b301
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-04-26 07:54:01 -0700

    Completed ServicePicture entity and wrote HelpRequestPicture entity.

--- Help.Domain.Core/ServiceAgg/Entities/HelpRequestPicture.cs
+++ Help.Domain.Core/ServiceAgg/Entities/HelpRequestPicture.cs
@@ -2,20 +2,20 @@
 
 namespace Help.Domain.Core.ServiceAgg.Entities
 {
-    public class ServicePicture : BaseEntity
+    public class HelpRequestPicture : BaseEntity
     {
-        public ServicePicture(string name, string title, string alt, long serviceId)
+        public HelpRequestPicture(string name, string title, string alt, long helpRequestId)
         {
-            Name = name; 
-            Title = title; 
-            Alt = alt; 
-            ServiceId = serviceId;
+            Name = name;
+            Title = title;
+            Alt = alt;
+            HelpRequestId = helpRequestId;
         }
 
         public string Name { get; private set; }
         public string Title { get; private set; }
         public string Alt { get; private set; }
-        public long ServiceId { get; private set; }
+        public long HelpRequestId { get; private set; }
 
         public void Edit(string name, string title, string alt)
         {
