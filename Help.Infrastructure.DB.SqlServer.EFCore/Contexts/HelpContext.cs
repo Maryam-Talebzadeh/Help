b059d6aa3cf15aa4603470a036b7317fb4199d2f
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-04-26 22:14:09 -0700

    Created HelpContext.

---
+++ Help.Infrastructure.DB.SqlServer.EFCore/Contexts/HelpContext.cs
@@ -0,0 +1,35 @@
+﻿using Help.Domain.Core.ServiceAgg.Entities;
+using Help.Infrastructure.DB.SqlServer.EFCore.Configurations.ServiceAgg;
+using Microsoft.EntityFrameworkCore;
+
+namespace Help.Infrastructure.DB.SqlServer.EFCore.Contexts
+{
+    public class HelpContext : DbContext
+    {
+        public HelpContext(DbContextOptions<HelpContext> options) :base(options)
+        {
+            
+        }
+
+        #region Service Agg
+
+        public DbSet<Service> Services { get; set; }
+        public DbSet<ServiceCategory> ServiceCategories { get; set; }
+        public DbSet<ServicePicture> ServicePictures { get; set; }
+        public DbSet<Skill> Skills { get; set; }
+        public DbSet<Proposal> Proposals { get; set; }
+        public DbSet<HelpRequest> HelpRequests { get; set; }
+        public DbSet<HelpRequestStatus> HelpRequestStatuses { get; set; }
+        public DbSet<HelpRequestPicture> HelpRequestPictures { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+
+        #endregion
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var assembly = typeof(HelpRequestConfiguration).Assembly;
+            modelBuilder.ApplyConfigurationsFromAssembly(assembly);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
