203d627da0bfc049ec9eda1fe7203a4457deb683
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-04-27 10:22:21 -0700

    Wrote WalletOperation Configuration.

---
+++ Help.Infrastructure.DB.SqlServer.EFCore/Configurations/WalletAgg/WalletOperationConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Help.Domain.Core.WalletAgg.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+
+namespace Help.Infrastructure.DB.SqlServer.EFCore.Configurations.WalletAgg
+{
+    public class WalletOperationConfiguration : IEntityTypeConfiguration<WalletOperation>
+    {
+        public void Configure(EntityTypeBuilder<WalletOperation> builder)
+        {
+            #region Relations 
+
+            builder.HasOne(wo => wo.Type)
+           .WithMany(t => t.Operations)
+           .HasForeignKey(wo => wo.TypeId);
+
+            #endregion
+        }
+    }
+}
