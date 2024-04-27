e5a45d4ea53b7ad8a76a3bc3ab92744cac26ed91
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-04-27 07:27:53 -0700

    Wrote CustomerPicture entity from Account Aggregate.

---
+++ Help.Domain.Core/AccountAgg/Entities/CustomerPicture.cs
@@ -0,0 +1,16 @@
+﻿
+
+using Base_Framework.Domain.Core.Entities;
+
+namespace Help.Domain.Core.AccountAgg.Entities
+{
+    public class CustomerPicture : Picture
+    {
+        public CustomerPicture(string name, string title, string alt, long customerId) : base(name, title, alt)
+        {
+            
+        }
+
+        public long CustomerId { get; private set; }
+    }
+}
