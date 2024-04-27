f82a917d858a88febfa6635b6e0142db7e027e5f
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-04-26 21:47:03 -0700

    Wrote HelpRequestStatus Configuration.

---
+++ Help.Infrastructure.DB.SqlServer.EFCore/Configurations/ServiceAgg/HelpRequestStatusConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Help.Domain.Core.ServiceAgg.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Help.Infrastructure.DB.SqlServer.EFCore.Configurations.ServiceAgg
+{
+    public class HelpRequestStatusConfiguration : IEntityTypeConfiguration<HelpRequestStatus>
+    {
+        public void Configure(EntityTypeBuilder<HelpRequestStatus> builder)
+        {
+            builder.Property(x => x.Title).HasMaxLength(50).IsRequired();
+            builder.Property(x => x.Description).HasMaxLength(500);
+        }
+    }
+}
