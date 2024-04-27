8dc7673fae0f00df5ef4ca4eff78b016f6f7e608
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-04-26 21:17:30 -0700

    Wrote ServicePicture configuration.

---
+++ Help.Infrastructure.DB.SqlServer.EFCore/Configurations/ServiceAgg/ServicePictureConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Help.Domain.Core.ServiceAgg.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Help.Infrastructure.DB.SqlServer.EFCore.Configurations.ServiceAgg
+{
+    public class ServicePictureConfiguration : IEntityTypeConfiguration<ServicePicture>
+    {
+        public void Configure(EntityTypeBuilder<ServicePicture> builder)
+        {
+            builder.Property(x => x.Title).HasMaxLength(50).IsRequired();
+            builder.Property(x => x.Name).HasMaxLength(100).IsRequired();
+            builder.Property(x => x.Alt).HasMaxLength(100).IsRequired();
+        }
+    }
+}
