028e9de27821c381ff0e7912f3e8e5fefde514dc
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-04-26 22:40:26 -0700

    I choose API for My EndPoint. so I created it and created a class to register local services.

---
+++ Help.EndPoints.API/ServiceConfigurations/LocalServiceConfigurations.cs
@@ -0,0 +1,17 @@
+﻿using Help.Infrastructure.DB.SqlServer.EFCore.Contexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace Help.EndPoints.API.ServiceConfigurations
+{
+    public class LocalServiceConfigurations
+    {
+        public static void Configure(IServiceCollection services, string connectionString)
+        {
+            #region DBContext
+
+            services.AddDbContext<HelpContext>(x => x.UseSqlServer(connectionString));
+
+            #endregion
+        }
+    }
+}
