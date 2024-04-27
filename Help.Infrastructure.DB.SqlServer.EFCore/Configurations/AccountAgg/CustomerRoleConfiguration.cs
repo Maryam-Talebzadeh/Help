266606fe0019376edfd7b5a6c76acdfd62b91a06
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-04-27 09:30:47 -0700

    Wrote CustomerRole Configuration.

---
+++ Help.Infrastructure.DB.SqlServer.EFCore/Configurations/AccountAgg/CustomerRoleConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Help.Domain.Core.AccountAgg.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Help.Infrastructure.DB.SqlServer.EFCore.Configurations.AccountAgg
+{
+    public class CustomerRoleConfiguration : IEntityTypeConfiguration<CustomerRole>
+    {
+        public void Configure(EntityTypeBuilder<CustomerRole> builder)
+        {
+            builder.Property(c => c.Title).HasMaxLength(50).IsRequired();
+
+            #region Relations
+
+            builder.HasMany(cr => cr.Comments)
+               .WithOne(c => c.CustomerRole)
+               .HasForeignKey(c => c.CustomerRoleId);
+
+            #endregion
+        }
+    }
+}
