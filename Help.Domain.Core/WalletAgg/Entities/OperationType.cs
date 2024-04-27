2fc62472b720ce8ecac90a6951662e8f1b30e339
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-04-27 10:04:10 -0700

    Wrote OperationType entity from Wallet Aggregate.

---
+++ Help.Domain.Core/WalletAgg/Entities/OperationType.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace Help.Domain.Core.WalletAgg.Entities
+{
+    public class OperationType
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+    }
+}
