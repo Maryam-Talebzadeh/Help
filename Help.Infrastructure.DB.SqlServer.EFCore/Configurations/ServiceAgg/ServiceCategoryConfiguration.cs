32121c372479bf54e75fdcdd259a4bd58f9c2dbe
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-04-26 21:13:10 -0700

    Wrote ServiceCategory configuration.

---
+++ Help.Infrastructure.DB.SqlServer.EFCore/Configurations/ServiceAgg/ServiceCategoryConfiguration.cs
@@ -0,0 +1,17 @@
+﻿
+
+using Help.Domain.Core.ServiceAgg.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Help.Infrastructure.DB.SqlServer.EFCore.Configurations.ServiceAgg
+{
+    public class ServiceCategoryConfiguration : IEntityTypeConfiguration<ServiceCategory>
+    {
+        public void Configure(EntityTypeBuilder<ServiceCategory> builder)
+        {
+            builder.Property(x => x.Title).HasMaxLength(50).IsRequired();
+            builder.Property(x => x.Description).HasMaxLength(500);
+        }
+    }
+}
