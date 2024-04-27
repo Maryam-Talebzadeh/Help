a9675e3ed5f534c05593971244d148f1a0b6234c
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-04-27 09:50:56 -0700

    I started to write Wallet aggregate. So at first I wrote the Wallet entity.

---
+++ Help.Domain.Core/WalletAgg/Entities/Wallet.cs
@@ -0,0 +1,16 @@
+﻿using Base_Framework.Domain.Core.Entities;
+
+
+namespace Help.Domain.Core.WalletAgg.Entities
+{
+    public class Wallet : BaseEntity
+    {
+        public Wallet(long customerId)
+        {
+            CustomerId = customerId;
+        }
+
+        public double Balance { get; private set; }
+        public long CustomerId  { get; private set; }
+    }
+}
