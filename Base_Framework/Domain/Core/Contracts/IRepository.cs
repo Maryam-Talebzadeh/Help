09fb34906be8c0caa53cecf8437594c4054cad63
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-04-28 07:45:14 -0700

    Wrote IRepository to use in different repositories.

---
+++ Base_Framework/Domain/Core/Contracts/IRepository.cs
@@ -0,0 +1,11 @@
+﻿using Base_Framework.Domain.Core.Entities;
+using System.Linq.Expressions;
+
+namespace Base_Framework.Domain.Core.Contracts
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        public bool IsExist(Expression<Func<T, bool>> expression);
+        public void Save();
+    }
+}
