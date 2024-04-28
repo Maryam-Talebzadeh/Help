993a322257e73e63ddf391f7813bb6fb92aa48ec
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-04-28 07:55:04 -0700

    Wrote BaseRepository to use it generally.

---
+++ Base_Framework/Infrastructure/DataAccess/BaseRepository_EFCore.cs
@@ -0,0 +1,32 @@
+﻿using Base_Framework.Domain.Core.Contracts;
+using Base_Framework.Domain.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Base_Framework.Infrastructure.DataAccess
+{
+    public class BaseRepository_EFCore<T> : IRepository<T> where T : BaseEntity
+    {
+        private readonly DbContext _context;
+
+        public BaseRepository_EFCore(DbContext context)
+        {
+            _context = context;
+        }
+
+        public bool IsExist(Expression<Func<T, bool>> expression)
+        {
+            return _context.Set<T>().Any(expression);
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+        public T Get(long id)
+        {
+            return _context.Set<T>().SingleOrDefault(e => e.Id == id);
+        }
+    }
+}
