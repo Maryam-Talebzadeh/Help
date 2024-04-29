2bfa85d0875fe42c6cdc8b52abc1e70b33781efe
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-04-29 10:25:40 -0700

    Wrote HelpRequestPictureRepository.

---
+++ Help.Infrastructure.DataAccess.Repos.EFCore/HelpServiceAgg/HelpRequestPictureRepository.cs
@@ -0,0 +1,31 @@
+﻿using Base_Framework.Infrastructure.DataAccess;
+using Help.Domain.Core.HelpServiceAgg.Data;
+using Help.Domain.Core.HelpServiceAgg.DTOs.HelpRequestPicture;
+using Help.Domain.Core.HelpServiceAgg.Entities;
+using Help.Infrastructure.DB.SqlServer.EFCore.Contexts;
+
+namespace Help.Infrastructure.DataAccess.Repos.EFCore.HelpServiceAgg
+{
+    public class HelpRequestPictureRepository : BaseRepository_EFCore<HelpRequestPicture>, IHelpRequestPictureRepository
+    {
+        private readonly HelpContext _context;
+
+        public HelpRequestPictureRepository(HelpContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public void Create(CreateHelpRequestPictureDTO command)
+        {
+            var picture = new HelpRequestPicture(command.Name, command.Title, command.Alt, command.HelpRequestId);
+            _context.HelpRequestPictures.Add(picture);
+        }
+
+        public void Edit(EditHelpRequestPictureDTO command)
+        {
+            var picture = Get(command.Id);
+            picture.Edit(command.Name, command.Title, command.Alt);
+        }
+    }
+    
+}
