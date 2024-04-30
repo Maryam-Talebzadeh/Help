3cc44c9d6489bba926bbb2cf92562ec48f77d808
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-04-29 17:15:33 -0700

    Wrote AddressRepository.

---
+++ Help.Infrastructure.DataAccess.Repos.EFCore/AccountAgg/AddressRepository.cs
@@ -0,0 +1,30 @@
+﻿using Base_Framework.Infrastructure.DataAccess;
+using Help.Domain.Core.AccountAgg.Data;
+using Help.Domain.Core.AccountAgg.DTOs.Address;
+using Help.Domain.Core.AccountAgg.Entities;
+using Help.Infrastructure.DB.SqlServer.EFCore.Contexts;
+
+namespace Help.Infrastructure.DataAccess.Repos.EFCore.AccountAgg
+{
+    public class AddressRepository : BaseRepository_EFCore<Address>, IAddressRepository
+    {
+        private readonly HelpContext _context;
+
+        public AddressRepository(HelpContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public void Create(CreateAddressDTO command)
+        {
+            var address = new Address(command.Description, command.CityId, command.StreetName, command.AlleyNumber);
+            _context.Addresses.Add(address);
+        }
+
+        public void Edit(EditAddressDTO command)
+        {
+            var address = Get(command.Id);
+            address.Edit(command.Description, command.CityId, command.StreetName, command.AlleyNumber);
+        }
+    }
+}
