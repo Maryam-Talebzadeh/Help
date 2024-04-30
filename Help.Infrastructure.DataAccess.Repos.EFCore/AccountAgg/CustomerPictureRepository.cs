c4e4dee514183ec028da8b2afd09cc90a020ace1
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-04-29 17:01:24 -0700

    Wrote CustomerPictureRepository.

---
+++ Help.Infrastructure.DataAccess.Repos.EFCore/AccountAgg/CustomerPictureRepository.cs
@@ -0,0 +1,30 @@
+﻿using Base_Framework.Infrastructure.DataAccess;
+using Help.Domain.Core.AccountAgg.Data;
+using Help.Domain.Core.AccountAgg.DTOs.CustomerPicture;
+using Help.Domain.Core.AccountAgg.Entities;
+using Help.Infrastructure.DB.SqlServer.EFCore.Contexts;
+
+namespace Help.Infrastructure.DataAccess.Repos.EFCore.AccountAgg
+{
+    public class CustomerPictureRepository : BaseRepository_EFCore<CustomerPicture>, ICustomerPictureRepository
+    {
+        private readonly HelpContext _context;
+
+        public CustomerPictureRepository(HelpContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public void Create(CreateCustomerPictureDTO command)
+        {
+            var picture = new CustomerPicture(command.Name, command.Title, command.Alt, command.CustomerID);
+            _context.CustomerPictures.Add(picture);
+        }
+
+        public void Edit(EditCustomerPictureDTO command)
+        {
+            var picture = Get(command.Id);
+            picture.Edit(command.Name, command.Title, command.Alt);
+        }
+    }
+}
