4f019925744172e5f0577135030c660b6fc3a88a
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-05-09 05:33:15 -0700

    Added OperationResult class for return type.

---
+++ Base_Framework/Domain/Services/OperationResult.cs
@@ -0,0 +1,31 @@
+﻿
+
+namespace Base_Framework.Domain.Services
+{
+    public class OperationResult<T>
+    {
+        public OperationResult(T obj)
+        {
+            IsSuccedded = false;
+            OperationReference = obj;
+        }
+
+        public bool IsSuccedded { get; set; }
+        public string Message { get; set; }
+        public T OperationReference { get; set; }
+
+        public OperationResult<T> Succedded(string message = "عملیات با موفقیت انجام شد")
+        {
+            IsSuccedded = true;
+            Message = message;
+            return this;
+        }
+
+        public OperationResult<T> Failed(string message)
+        {
+            IsSuccedded = false;
+            Message = message;
+            return this;
+        }
+    }
+}
