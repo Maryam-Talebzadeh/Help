9f020c2031ad7e1011a84191576943ed1cdcfff3
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-05-13 10:58:41 -0700

    Add Index Page of admin.

---
+++ Help.EndPoints.RazorPage/Areas/Administration/Pages/HelpServiceAgg/HelpRequest/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Help.EndPoints.RazorPage.Areas.Administration.Pages.HelpServiceAgg.HelpRequest
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
