31f9a6259d9f141acbc358b057125c71e7f2c55b
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-05-19 21:21:09 -0700

    Added The main layout and it's assets.

---
+++ Help.EndPoints.RazorPage/ViewComponents/MenuViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Help.EndPoints.RazorPage.ViewComponents
+{
+    public class MenuViewComponent : ViewComponent
+    {
+        public MenuViewComponent()
+        {
+        }
+
+        public IViewComponentResult Invoke()
+        {
+
+            return View();
+        }
+    }
+}
