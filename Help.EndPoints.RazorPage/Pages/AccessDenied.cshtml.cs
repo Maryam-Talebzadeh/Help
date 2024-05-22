b27d9a4b2ba34beb43de393926b0e15583427dec
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-05-22 10:21:43 -0700

    Added AccessDenied page.

---
+++ Help.EndPoints.RazorPage/Pages/AccessDenied.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Help.EndPoints.RazorPage.Pages
+{
+    public class AccessDeniedModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
